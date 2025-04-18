1f72d6a78a33a419699ae75ed5b5a6d815f532ba
Ana Julia <cardosomoseranajulia@gmail.com>
2025-04-18 19:06:04 -0300

    primeira pag com html + js. validação de numeros

--- exercicioaula2/primeiro.js
+++ exercicioaula2/primeiro.js
@@ -1,6 +1,20 @@
-function validarNumero()
-{
-    //criar uma variavel p receber o valor digitado pelo usuarip
-    let numero = document.getElementById("numero").value;
-    let mensagem =document.getElementById("mensagem");
+let numeroInput= document.getElementById("numero");
+let mensagemText =document.getElementById("mensagem");
+function validarNumero(){
+    let numero = parseInt(numeroInput.value);
+    if (!numeroInput.value) {
+        mensagemText.innerText = "Por favor, digite um número.";
+        mensagemText.style.color = "red";
+        return; 
+    }
+    if (numero>10){
+        mensagemText.innerText = "O número é maior que 10";
+        mensagemText.style.color="green";
+    } else if (numero > 5) {
+        mensagemText.innerText = "O número é maior que 5, mas menor ou igual 10";
+        mensagemText.style.color="orange";
+    } else {
+        mensagemText.innerText = "O número é 5 ou menor";
+        mensagemText.style.color="blue";
+    }
 }
